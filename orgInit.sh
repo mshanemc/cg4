e784deb81ed1d649966e31d47ebcd5de76dd99c7
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-29 10:42:05 -0700

    multiplex and self-reg

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
 sfdx shane:github:package:install --githubuser mshanemc --repo volvo-base
+sfdx shane:github:package:install --githubuser mshanemc --repo platform-event-multiplexer-client
 sfdx force:user:permset:assign -n VehiclesPerms
 sfdx force:apex:execute -f scripts/init.cls
 
@@ -8,3 +9,4 @@ sfdx force:user:permset:assign -n drive
 sfdx force:apex:execute -f scripts/initLocal.cls
 # sfdx shane:iot:enable --insights
 sfdx force:org:open
+sfdx shane:iot:activation -n Scheduled_Maintenance -r
