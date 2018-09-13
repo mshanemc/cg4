23a35763dd8d5e986852e9b22e9228f3cbd47d35
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-09-13 06:43:35 -0500

    move org:open to last (buy some time on the domain?)

--- orgInit.sh
+++ orgInit.sh
@@ -10,7 +10,8 @@ sfdx force:apex:execute -f scripts/initLocal.cls
 sfdx force:apex:execute -f scripts/initMultiplexer.cls
 
 # sfdx shane:iot:enable --insights
-sfdx force:org:open
 sfdx shane:iot:activation -n Scheduled_Maintenance -r
 
 sfdx force:data:record:create -s Drive__e -v "Vin__c='YV1DZ8256C2271234' Ending_Odometer__c=200"
+
+sfdx force:org:open
